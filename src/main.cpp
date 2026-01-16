72b8867744e2948014d288fb5208be064535d9f5
Dan Mihovch <dmihovch@gmail.com>
2026-01-16 15:52:18 -0500

    going through docs

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,12 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <chrono>
+#include <thread>
+
+void framebuffer_resize_handler(GLFWwindow* win, int width, int height){
+	glViewport(0,0,width,height);
+}
 
 int main() {
     if (!glfwInit()) {
@@ -27,14 +33,31 @@ int main() {
         return -1;
     }
 
+
+	glViewport(0,0,800,600);
+	glfwSetFramebufferSizeCallback(window, framebuffer_resize_handler);
+	//on input section of opengl docs
+	
+
     glEnable(GL_DEPTH_TEST);
 
+
+	std::srand(0);
+	float cvals[4];
     while (!glfwWindowShouldClose(window)) {
-        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+	
+		
+		for(int i = 0; i<4; ++i){
+			cvals[i] = (static_cast<float>(std::rand())/RAND_MAX);
+		}
+
+        glClearColor(cvals[0], cvals[1], cvals[2], cvals[3]);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+		std::this_thread::sleep_for(std::chrono::seconds(1));
         glfwSwapBuffers(window);
         glfwPollEvents();
+
     }
 
     glfwDestroyWindow(window);
